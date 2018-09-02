c963ccc52649966fdabad2f4a20644e8cd2b5e8b
Gino <38804963+EugenioAntonioBonanno@users.noreply.github.com>
2018-09-02 15:52:53 +0800

    updated

--- Hidden Message.py
+++ Hidden Message.py
@@ -1,13 +1,15 @@
 
+
 # coding: utf-8
 
-# In[8]:
+# In[ ]:
 
 
 # Set the passcode the user needs to enter as their name to activate program.
 
 passcode = "a"
 
+messages = []  # Need to find a way for this not to reset the list each time the code is run.
 
 #Get the user to enter a passcode, check if its compatible, if not kick user.
 
@@ -24,7 +26,7 @@ while True:
 #If user passes the test welcome them back, and find out if they wish to receive or leave a message    
     
     if passcode == login:
-        print("Welcome back Commander",name +"!")
+        print("Welcome back Commander",name.title() +"!")
     
 # Get user input for action and make sure that its either receive, or leave.
     
@@ -39,19 +41,36 @@ while True:
         else:
             print("Sorry but we only deal in certainties, please enter either 'receive' or 'leave':D!")
         
-#Allow user to leave a message, save message in a list, hash message, display hash to be used as a key.        
+#Allow user to leave a message, save message in a list, hash message, display hash to be used as a key.  
 
-
-    messages = []
 
     if action.lower() == "leave":
         message = input("So what exactly is so important that you couldn't say it anywhere else? : ")
         messages.append(message)
+        location = messages.index(message)
         message = ""
-        print(messages[0])
-        
+        lock = hash(messages[location])
+        print(key)
+        print(messages[location])
+        print(location)
         
         break
+        
+        
+# This is the part of the code to check your messages   
+# Check the hash against the hashes of the items in the list, if its the same display the message, and change the messages
+   
+    if action.lower() == "receive":
+        key = input("What key were you given?")
+        for item in messages:
+            if hash(item) == key:
+                print(item)
+                item = "Sorry we can't find what your looking for"
+                break
+    else:
+        print("Sorry that code doesn't exist, it may have been used already :(. \n Try torturing the information out of your friend!)")
+        break 
+            
         
     
     
